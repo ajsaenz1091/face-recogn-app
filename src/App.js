3325daf6559d50b884debb1ddd1fe60a501db2c3
Alberto Gonzalez <ajsaenz1091@gmail.com>
2020-08-25 18:41:37 -0400

    State and onInptChange event listener test working

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -32,7 +44,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
       </div>
     );
   }
