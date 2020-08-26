be0f7351d548e57a3ff0d949eafd5a22d6d05330
Alberto Gonzalez <ajsaenz1091@gmail.com>
2020-08-25 22:23:00 -0400

    Register Component routing test 1 working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
-// import Register from './components/Register/Register';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -86,9 +86,8 @@ class App extends Component {
           params={particlesOptions}
         />
         <Navigation onRouteChange={this.onRouteChange}/>
-        { this.state.route === 'signin' 
-        ? <Signin onRouteChange={this.onRouteChange} />
-        : <div >
+        { this.state.route === 'home' 
+        ? <div >
             <Logo />
             <Rank />
             <ImageLinkForm 
@@ -96,6 +95,12 @@ class App extends Component {
               onImageSubmit={this.onImageSubmit} />
             <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box}/>
           </div>
+        : (
+            this.state.route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange} />
+          )
+
         }
       </div>
     );
